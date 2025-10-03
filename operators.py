a861b4ae5de16ce28e7814b2f7e8097132033b1e
manikandann31 <manikandanpro31@gmail.com>
2025-10-03 12:53:32 +0530

    new project file

---
+++ operators.py
@@ -0,0 +1,21 @@
+a=21
+b=10
+#arithmetic
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+#comparison 
+print(a==b)
+print(a!=b)
+print (a<b)
+print(a>b)
+#logical operator (AND && , OR||)
+x= True
+y= False 
+print(x and y)
+print(x or y)
+
