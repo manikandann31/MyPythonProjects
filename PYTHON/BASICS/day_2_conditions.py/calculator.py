79e1b7af135c5835b6560f37acf99d395f389646
manikandann31 <manikandanpro31@gmail.com>
2025-12-20 17:35:35 +0530

    a day 2 calculator program with conditionals

---
+++ PYTHON/BASICS/day_2_conditions.py/calculator.py
@@ -0,0 +1,17 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+op=input("what operation do you want to do(add,sub,mul,div,mod) with the two numbers: ")
+if op.lower()=="add":
+    print(a+b)
+elif op.lower() =="sub":
+    print(a-b)
+elif op.lower() =="mul":
+    print(a*b)
+elif op.lower() =="div":
+    print(a/b)
+elif op.lower() =="mod":
+    print(a%b)
+else:
+    print("Enter a valid operation")
+
+
